75e53e0ee99e6cc1216ecfd7cb84738e4ad02d8b
Büşra <bakpinar@nettsi.com>
2022-04-03 14:31:01 +0300

    Tidy-ups and corrections

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
 # mizu_signin_automation
+
+This project automates the functional testing of the member login page of the Mizu website. In this project, Selenium Webdriver was used as a web automation tool, JUnit for unit testing, Java as a software language, Visual Studio Code as a code editor and chrome driver as a driver.
+
+Selenium WebDriver is a browser automation framework that accepts commands and sends them to a browser. It is implemented via a browser-specific driver. It communicates directly with the scanner and controls it. In order to perform each step of the test case, whose scenario was written with Selenium Webdriver, the relevant areas of the web page were accessed. Each test case is written as a function.
+
+The input and output control of every test-case function written in Junit is provided using JUnit's notations.
+
+Java is a program that can run independently of any computer architecture and platforms. Java, which has many different features; It is an open source, object-oriented, multi-functional, high-level language, highly efficient, platform-independent, step-by-step application.
+
+This project is built with Java-Maven. In order for the project to be compiled correctly, the Chrome Driver added to the project must be 100 or higher.
+
+#How to run JUnit test cases from the command line?
+
+Maven way
+
+If you use Maven, you can run the following command to run all your test cases:
+
+mvn clean test
+Or you can run a particular test as below
+
+mvn clean test -Dtest=your.package.TestClassName
+mvn clean test -Dtest=your.package.TestClassName#particularMethod
+If you would like to see the stack trace (if any) in the console instead of report files in the target\surefire-reports folder, set the user property surefire.useFile to false. For example:
+
+mvn clean test -Dtest=your.package.TestClassName -Dsurefire.useFile=false
+
+
+Normal way
+
+If you do not use Maven, or Gradle or Ant, you can follow the following way:
+
+First of all, you need to compile your test cases. For example (in Linux):
+
+javac -d /absolute/path/for/compiled/classes -cp /absolute/path/to/junit-4.12.jar /absolute/path/to/TestClassName.java
+
+Then run your test cases. For example:
+
+java -cp /absolute/path/for/compiled/classes:/absolute/path/to/junit-4.12.jar:/absolute/path/to/hamcrest-core-1.3.jar org.junit.runner.JUnitCore your.package.TestClassName
+
+
+
---
+++ src/test/java/com/ciceksepeti/mizu/SigninTest.java
@@ -0,0 +1,326 @@
+package com.ciceksepeti.mizu;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.util.Iterator;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.jupiter.api.*;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.PageLoadStrategy;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+public class SigninTest {
+
+    WebDriver driver = null;
+
+    private WebElement assertWaitVisible(By by){
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        try {
+            return wait.until(ExpectedConditions.visibilityOfElementLocated(by));
+        } catch (Exception e) {
+            fail();
+            return null;
+        }
+    }
+
+    private void assertWaitRedirect(String url_contains){
+  
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        try {
+            wait.until(ExpectedConditions.urlContains(url_contains));
+        } catch (Exception e) {
+            fail();
+        }
+    }
+
+    private void assertWaitNotVisible(By by){
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        try {
+            wait.until(ExpectedConditions.visibilityOfElementLocated(by));
+            fail();
+        } catch (Exception e) {}
+    }
+
+    @BeforeEach
+    public void testCaseSetup() {
+       
+        System.setProperty("webdriver.chrome.driver", "src/drivers/chromedriver");
+        ChromeOptions chromeOptions = new ChromeOptions();
+
+        chromeOptions.setPageLoadStrategy(PageLoadStrategy.EAGER);
+        driver = new ChromeDriver(chromeOptions);
+        //driver.manage().window().maximize();
+        driver.get("https://www.mizu.com/en-mx/login");
+    }
+
+    @AfterEach
+    public void testCaseTeardown() {
+        driver.quit();
+    }
+
+    // Scenario: The correct user name and password are given to the login page, and the login button is clicked.
+    // Expectation: Login should be successful.
+    @Test
+    public void TC01LoginValidUsernameAndValidPassword() {
+
+        WebElement setEmailAdress = driver.findElement(By.xpath("//input[@id='EmailLogin']"));
+        setEmailAdress.click();
+        setEmailAdress.sendKeys("clarklisa686@gmail.com");
+
+        WebElement setPassword = driver.findElement(By.xpath("//input[@id='Password']"));
+        setPassword.click();
+        setPassword.sendKeys("T1e2s3t4.");
+
+        WebElement signinButton = driver.findElement(By.xpath("//button[text()='Sign In']"));
+        signinButton.click();
+
+        // Message that is going to be displayed when the input is unable to be handled
+        assertWaitNotVisible(By.xpath("//*[contains(text(), 'E-mail address or password is incorrect. Please check your information and try again.')]"));
+
+        // The "Login" of the URL includes the "Login" input page and is provided to the test of the test with assertfalse
+        assertFalse(driver.getCurrentUrl().contains("login"));
+
+        // The end of the URL address must be '/ account / edit-information' when logged in. 
+        // * The sign in the URL received with CSSSelector was used to prevent the entry control to be affected by the use of the site in different languages.
+        WebElement loginSuccessCheck = driver.findElement(By.cssSelector("a[href*='/account/edit-information']"));
+        assertTrue(loginSuccessCheck != null);
+
+    }
+
+    // Scenario: The correct user name and incorrect password are given to the login page, and the login button is clicked.
+    // Expectation: Login should be unsuccessful.
+    @Test
+    public void TC02LoginValidUsernameAndInvalidPassword() {
+
+        WebElement setEmailAdress = driver.findElement(By.xpath("//input[@id='EmailLogin']"));
+        setEmailAdress.click();
+        setEmailAdress.sendKeys("clarklisa686@gmail.com");
+
+        WebElement setPassword = driver.findElement(By.xpath("//input[@id='Password']"));
+        setPassword.click();
+        setPassword.sendKeys("Invalidpswd.");
+
+        WebElement signinButton = driver.findElement(By.xpath("//button[text()='Sign In']"));
+        signinButton.click();
+
+        // If the URL address contains "Login", it means that it is on the input screen, and the test is ensured by assertTrue
+        assertTrue(driver.getCurrentUrl().contains("login"));
+
+        // The Pop-up is expected to be displayed with the negative input and the relevant message is not received, the test is provided in the catch blog
+        assertWaitVisible(By.xpath("//*[contains(text(), 'E-mail address or password is incorrect. Please check your information and try again.')]"));
+     
+    }
+
+    // Scenario: The incorrect user name and correct password are given to the login page, and the login button is clicked.
+    // Expectation: Login should be unsuccessful.
+    @Test
+    public void TC03LoginInvalidUsernameAndValidPassword() {
+
+        WebElement setEmailAdress = driver.findElement(By.xpath("//input[@id='EmailLogin']"));
+        setEmailAdress.click();
+        setEmailAdress.sendKeys("clarklisa686868@gmail.com");
+
+        WebElement setPassword = driver.findElement(By.xpath("//input[@id='Password']"));
+        setPassword.click();
+        setPassword.sendKeys("T1e2s3t4.");
+
+        WebElement signinButton = driver.findElement(By.xpath("//button[text()='Sign In']"));
+        signinButton.click();
+
+        // If the URL address contains "Login", it means that it is on the input screen, and the test is ensured by assertTrue
+        assertTrue(driver.getCurrentUrl().contains("login"));
+
+        // The Pop-up is expected to be displayed with the negative input and the relevant message is not received, the test is provided in the catch blog
+        assertWaitVisible(By.xpath("//*[contains(text(), 'E-mail address or password is incorrect. Please check your information and try again.')]"));
+
+    }
+
+    // Scenario: The incorrect user name and password are given to the login page, and the login button is clicked.
+    // Expectation: Login should be unsuccessful.
+    @Test
+    public void TC04LoginInvalidUsernameAndInvalidPassword() {
+
+        WebElement setEmailAdress = driver.findElement(By.xpath("//input[@id='EmailLogin']"));
+        setEmailAdress.click();
+        setEmailAdress.sendKeys("clarklisa686868@gmail.com");
+
+        WebElement setPassword = driver.findElement(By.xpath("//input[@id='Password']"));
+        setPassword.click();
+        setPassword.sendKeys("Invalidpswd.");
+
+        WebElement signinButton = driver.findElement(By.xpath("//button[text()='Sign In']"));
+        signinButton.click();
+
+        // If the URL address contains "Login", it means that it is on the input screen, and the test is ensured by assertTrue
+        assertTrue(driver.getCurrentUrl().contains("login"));
+
+        // The Pop-up is expected to be displayed with the negative input and the relevant message is not received, the test is provided in the catch blog
+        assertWaitVisible(By.xpath("//*[contains(text(), 'E-mail address or password is incorrect. Please check your information and try again.')]"));
+
+    }
+
+    // Scenario: The blank user name are given to the login page, and the login button is clicked.
+    // Expectation: Login should be unsuccessful.
+    @Test
+    public void TC05LoginBlankUsername() {
+
+        WebElement setEmailAdress = driver.findElement(By.xpath("//input[@id='EmailLogin']"));
+        setEmailAdress.click();
+        setEmailAdress.sendKeys("");
+
+        WebElement setPassword = driver.findElement(By.xpath("//input[@id='Password']"));
+        setPassword.click();
+        setPassword.sendKeys("T1e2s3t4.");
+
+        WebElement signinButton = driver.findElement(By.xpath("//button[text()='Sign In']"));
+        signinButton.click();
+
+        // If the URL address contains "Login", it means that it is on the input screen, and the test is ensured by assertTrue
+        assertTrue(driver.getCurrentUrl().contains("login"));
+
+        assertWaitVisible(By.xpath("//span[contains(text(), 'Required field.')]"));
+
+    }
+
+    // Scenario: The blank password are given to the login page, and the login button is clicked.
+    // Expectation: Login should be unsuccessful.
+    @Test
+    public void TC06LoginBlankPassword() {
+
+        WebElement setEmailAdress = driver.findElement(By.xpath("//input[@id='EmailLogin']"));
+        setEmailAdress.click();
+        setEmailAdress.sendKeys("clarklisa686@gmail.com");
+
+        WebElement setPassword = driver.findElement(By.xpath("//input[@id='Password']"));
+        setPassword.click();
+        setPassword.sendKeys("");
+
+        WebElement signinButton = driver.findElement(By.xpath("//button[text()='Sign In']"));
+        signinButton.click();
+
+        // If the URL address contains "Login", it means that it is on the input screen, and the test is ensured by assertTrue
+        assertTrue(driver.getCurrentUrl().contains("login"));
+
+        assertWaitVisible(By.xpath("//span[contains(text(), 'Required field.')]"));
+
+    }
+
+    // Scenario: The blank user name and password are given to the login page, and the login button is clicked.
+    // Expectation: Login should be unsuccessful.
+    @Test
+    public void TC07LoginBlankUsernameAndLoginBlankPassword() {
+
+        WebElement setEmailAdress = driver.findElement(By.xpath("//input[@id='EmailLogin']"));
+        setEmailAdress.click();
+        setEmailAdress.sendKeys("");
+
+        WebElement setPassword = driver.findElement(By.xpath("//input[@id='Password']"));
+        setPassword.click();
+        setPassword.sendKeys("");
+
+        WebElement signinButton = driver.findElement(By.xpath("//button[text()='Sign In']"));
+        signinButton.click();
+
+        // If the URL address contains "Login", it means that it is on the input screen, and the test is ensured by assertTrue
+        assertTrue(driver.getCurrentUrl().contains("login"));
+
+        assertWaitVisible(By.xpath("//span[contains(text(), 'Required field.')]"));
+
+    }
+
+    // Scenario: Click the "Login with Gmail" button.
+    // Expectation: Login should be successful
+    @Test
+    public void TC08LoginWithFacebook() throws InterruptedException  {
+
+        WebElement loginWithFacebookButton = driver.findElement(By.xpath("//span[contains(text(), 'Sign in with Facebook') and contains(@class, 'login__social-text')]"));
+        loginWithFacebookButton.click();
+        
+  
+        String parentWindowHandler = driver.getWindowHandle(); // Store your parent window
+        String subWindowHandler = null;
+
+
+        while(driver.getWindowHandles().size() == 1);
+        Set<String> handles = driver.getWindowHandles(); // get all window handles
+        Iterator<String> iterator = handles.iterator();
+        while (iterator.hasNext()){
+            subWindowHandler = iterator.next();
+            System.out.println(subWindowHandler);
+        }
+        driver.switchTo().window(subWindowHandler); // switch to popup window
+                
+        WebElement setEmailForFacebook = driver.findElement(By.id("email"));
+        setEmailForFacebook.click();
+        setEmailForFacebook.sendKeys("notstick61@gmail.com");
+
+        WebElement setPasswordForFacebook = driver.findElement(By.id("pass"));
+        setPasswordForFacebook.click();
+        setPasswordForFacebook.sendKeys("test123test");
+
+        WebElement facebookLoginButton = driver.findElement(By.id("loginbutton"));
+        facebookLoginButton.click();
+
+        driver.switchTo().window(parentWindowHandler);  // switch back to parent window
+
+        driver.manage().timeouts().implicitlyWait(10000, TimeUnit.MILLISECONDS);
+        // The "Login" of the URL includes the "Login" input page and is provided to the test of the test with assertfalse
+        assertFalse(driver.getCurrentUrl().contains("login"));
+
+        // The end of the URL address must be '/ account / edit-information' when logged in. 
+        // * The sign in the URL received with CSSSelector was used to prevent the entry control to be affected by the use of the site in different languages.
+        WebElement loginSuccessCheck = driver.findElement(By.cssSelector("a[href*='/account/edit-information']"));
+        assertTrue(loginSuccessCheck != null);
+
+
+    }
+
+    // Scenario: Click the "Login with Gmail" button.
+    // Expectation: Login should be successful
+    @Test
+    public void TC09LoginWithGmail() throws InterruptedException {
+        System.out.println("after-1");
+        driver.manage().timeouts().pageLoadTimeout(5, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(2000, TimeUnit.MILLISECONDS);
+        WebElement loginWithGmailButton = driver.findElement(By.xpath("//span[contains(text(), 'Sign in with Google') and contains(@class, 'login__social-text login__social-google-text')]"));
+        loginWithGmailButton.click();
+        System.out.println(driver.getCurrentUrl());
+        System.out.println("after0");
+        System.out.flush();
+        driver.manage().timeouts().implicitlyWait(2000, TimeUnit.MILLISECONDS);
+        assertWaitRedirect("google");
+        System.out.println("after1");
+        WebElement setEmailForGmail = assertWaitVisible(By.id("identifierId"));
+        setEmailForGmail.click();
+        setEmailForGmail.sendKeys("clarklisa686@gmail.com");
+        System.out.println("after2");
+
+        WebElement forwardButton = driver.findElement(By.xpath("//span[contains(text(), 'İleri') and contains(@class, 'VfPpkd-vQzf8d')]"));
+        forwardButton.click();
+
+        WebElement  setPasswordForGmail = assertWaitVisible(By.xpath("//*[contains(@type, 'password') and contains(@class, 'whsOnd zHQkBf')]"));
+
+        // WebElement setPasswordForGmail = driver.findElement(By.xpath("//*[contains(@type, 'password') and contains(@class, 'whsOnd zHQkBf')]"));
+        setPasswordForGmail.click();
+        setPasswordForGmail.sendKeys("T1e2s3t4.");
+        forwardButton = driver.findElement(By.xpath("//span[contains(text(), 'İleri') and contains(@class, 'VfPpkd-vQzf8d')]"));
+
+        forwardButton.click();
+
+        // The "Login" of the URL includes the "Login" input page and is provided to the test of the test with assertfalse
+        assertFalse(driver.getCurrentUrl().contains("login"));
+
+    }
+
+}
